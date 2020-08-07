50f523fa41d475bde0e19643d852477976cd3af5
Charul090 <baricharul9@hotmail.com>
2020-08-07 20:36:13 +0530

    Added deletion

--- pin-input/src/Components/TypeInput.jsx
+++ pin-input/src/Components/TypeInput.jsx
@@ -11,7 +11,8 @@ export default class TypeInput extends Component {
         height: "50px",
         width: "50px",
         margin: "5px",
-      }
+        textAlign:"center"
+      },
     }
 
     this.values = new Array(this.props.length).fill("")
@@ -19,31 +20,45 @@ export default class TypeInput extends Component {
 
   }
 
-  handlechange=(e,i)=>{
+  handlechange = (e, i) => {
     console.log(this.values)
+
     this.values[i] = e.target.value
-    
-    if(i === this.elements.length-1 && this.values[i].length === 1){
-      this.elements[i].blur()
+
+    if (i === this.elements.length - 1 && this.values[i].length > 1) {
+      e.target.value = this.values[i][0]
+      this.values[i] = this.values[i][0]
     }
 
-    if(this.values[i].length > 0 && i < this.elements.length-1){
-        this.elements[i+1].focus()
+    if (this.values[i].length > 0 && i < this.elements.length - 1) {
+      this.elements[i + 1].focus()
     }
 
   }
 
+  handleDelete = (e, i) => {
+    if (e.keyCode === 8 && i === this.values.length - 1 && e.target.value === "") {
+      e.target.value = ""
+      this.values[i] = e.target.value
+      this.elements[i-1].focus()
+    }
+    else {
+      if (e.keyCode === 8 && i > 0 && e.target.value === "") {
+        this.elements[i - 1].focus()
+      }
+    }
+  }
+
 
   render() {
-    console.log(this.values)
     return (
       <div>
         {
-          this.values.map((elem,i)=>{
+          this.values.map((elem, i) => {
             return (
-            <>
-              <input style={this.style.input} ref={input => (this.elements[i] = input)} onChange={(e)=>this.handlechange(e,i)}/>
-            </>
+              <>
+                <input style={this.style.input} onKeyDown={(e) => this.handleDelete(e, i)} ref={input => (this.elements[i] = input)} onChange={(e) => this.handlechange(e, i)} />
+              </>
             )
           })
         }
