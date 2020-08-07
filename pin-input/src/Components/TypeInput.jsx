81a3c812b649587671a96862c4148558125f650a
Charul090 <baricharul9@hotmail.com>
2020-08-07 18:22:36 +0530

    Adding done

--- pin-input/src/Components/TypeInput.jsx
+++ pin-input/src/Components/TypeInput.jsx
@@ -1,46 +1,61 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { Component } from 'react'
+import PropTypes from "prop-types"
 
-const style = {
-  input: {
-    border: "1px solid black",
-    height: "50px",
-    width: "50px",
-    margin: "5px",
-  },
-};
-const TypeInput = () => {
-  const [inputVal, setinput] = useState({
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-  });
 
-  const [focus, setFocus] = useState({
-    1: true,
-    2: false,
-    3: true,
-    4: true,
-  });
+export default class TypeInput extends Component {
+  constructor(props) {
+    super(props)
+    this.style = {
+      input: {
+        border: "1px solid black",
+        height: "50px",
+        width: "50px",
+        margin: "5px",
+      }
+    }
 
-  //   useEffect(() => {
-  //     effect;
-  //     return () => {
-  //       cleanup;
-  //     };
-  //   }, [input]);
-  return (
-    <div>
-      <input
-        type="text"
-        name="1"
-        // onChange={}
-        autoFocus={focus[1]}
-        style={style.input}
-      />
-      <input type="text" name="2" autoFocus={focus[2]} style={style.input} />
-    </div>
-  );
-};
+    this.values = new Array(this.props.length).fill("")
+    this.elements = []
 
-export default TypeInput;
+  }
+
+  handlechange=(e,i)=>{
+    console.log(this.values)
+    this.values[i] = e.target.value
+    
+    if(i === this.elements.length-1 && this.values[i].length === 1){
+      this.elements[i].blur()
+    }
+
+    if(this.values[i].length > 0 && i < this.elements.length-1){
+        this.elements[i+1].focus()
+    }
+
+  }
+
+
+  render() {
+    console.log(this.values)
+    return (
+      <div>
+        {
+          this.values.map((elem,i)=>{
+            return (
+            <>
+              <input style={this.style.input} ref={input => (this.elements[i] = input)} onChange={(e)=>this.handlechange(e,i)}/>
+            </>
+            )
+          })
+        }
+      </div>
+    );
+  }
+}
+
+TypeInput.propTypes = {
+  length: PropTypes.number,
+}
+
+TypeInput.defaultProps = {
+  length: 4
+}
